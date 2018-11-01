475cdc4e945e9117b5ab4f3d50bdb7518c134d41
root <root@ords.(none)>
2018-11-01 20:57:01 +0700

    push code

--- oracle-installations/oracle-silent-installations.sh
+++ oracle-installations/oracle-silent-installations.sh
@@ -1,3 +1,3 @@
 ORACLE_INSTALLER=/stage/database/runInstaller
 ORACLE_RESPOND_FILE='/home/oracle/db11R2.rsp'
-[ -f $ORACLE_INSTALLER ] && su oracle -c "$ORACLE_INSTALLER -silent -responseFile $ORACLE_RESPOND_FILE" || echo "Cannot install oracle db"
+[ -f $ORACLE_INSTALLER ] && su oracle -c "DISPLAY=0.0.0.0:0.0; $ORACLE_INSTALLER -silent -responseFile $ORACLE_RESPOND_FILE" || echo "Cannot install oracle db"
