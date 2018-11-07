513167efbd31082c9a3d997c827d6a265b1772ad
root <root@mycentos6.(none)>
2018-11-07 11:28:23 +0700

    push code

---
+++ oracle-installations/oracle-db-12c-preinstallation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NETWORK_DEVICE='eth1'
+STORAGE_DEVICE='sdb1'
+IP_ADDR="$(ip addr show $NETWORK_DEVICE | grep -Po 'inet \K[\d.]+')"
+HOST_NAME='orcl12c'
+DATA_DIR='/data'
+
+# Check mount storage
+if [[ $(df -hT | grep '/dev/sdb1') ]]; then
+  echo "Check storageSTORAGE_DEVICE ... OK"
+else
+  echo 'Mounting storage...'
+  mount "/dev/$STORAGE_DEVICE" "$DATA_DIR" && echo 'Mount ... OK' || echo "Mount ... Fail"
+fi
