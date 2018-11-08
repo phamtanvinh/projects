7835579d59e387c93addfada17620227ee0dfd4b
phamtanvinh <phamtanvinh.me@gmail.com>
2018-11-08 20:50:44 +0700

    [2018-11-08 20:50] centos6 root: push code

--- linux/bin/centos-package-installation.sh
+++ linux/bin/centos-package-installation.sh
@@ -1,8 +1,10 @@
-CENTOS_PACKAGE_LIST='config/centos-package-list'
+CENTOS_PACKAGE_LIST='../config/centos-package-list'
+
 if [[ -f "$CENTOS_PACKAGE_LIST" ]]; then
-  source centos-package-list
+  source "$CENTOS_PACKAGE_LIST"
 else
   echo "$CENTOS_PACKAGE_LIST not found" && exit 1
+fi
 
 for GROUP in "${GROUP_LIST[@]}"; do
   yum groupinstall -y $GROUP 2>/dev/null || echo "$GROUP is installed"
