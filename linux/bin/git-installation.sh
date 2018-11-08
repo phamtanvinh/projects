ece2e8101f9b717b00826eaec785f8f7d4824944
phamtanvinh <phamtanvinh.me@gmail.com>
2018-11-08 21:49:24 +0700

    [2018-11-08 21:49] centos6 root: push code

--- linux/bin/git-installation.sh
+++ linux/bin/git-installation.sh
@@ -1,19 +1,29 @@
+GIT_ENV='../config/git_env'
 
-GIT_VERSION='2.9.5'
-GIT_SOURCE="https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz"
+if [[ -f "$GIT_ENV" ]]; then 
+  source "$GIT_ENV"
+else
+  echo "$GIT_ENV not found"
+  exit 1
+fi
 
+GIT_DIR="git-$GIT_VERSION"
 
-yum install -y curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
+for PACKAGE in "${GIT_PACKAGE_LIST[@]}"; do
+  printf "Installing %s ..." "$PACKAGE" && yum install -y $PACKAGE &>/dev/null &&echo 'OK'
+done
+
 (
     rm -rf /usr/local/git
-    rmdir /tmp/git*
+    [[ -d "/tmp/$GIT_DIR" ]] && rm -rf "/tmp/$GIT_DIR"
     sed -ci 's/.*\(\/usr\/local\/git\/bin\).*//' /etc/bashrc
     cd /tmp
     [[ -f ${GIT_SOURCE##*/} ]] && echo "Using exists ${GIT_SOURCE##*/}"|| wget $GIT_SOURCE
-    tar xzf ${GIT_SOURCE##*/}
-    cd git-2.9.5
+    tar xzf ${GIT_SOURCE##*/} && cd "$GIT_DIR"|| exit 1
     make prefix=/usr/local/git all
     make prefix=/usr/local/git install
     echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
-)
-source /etc/bashrc
+    rm -rf "/tmp/$GIT_DIR"
+    echo "Install done"
+) || { echo "Install git-$GIT_VERSION fail" && exit 1; }
+echo "Activate git-$GIT_VERSION: source /etc/bashrc"
